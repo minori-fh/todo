2c89c7aaa76628420e3d8e18924aa8c4b6bd76c2
Minori Hashimoto <minori.fh@gmail.com>
2024-10-26 19:32:19 -0700

    add test and delete functionality

--- backend/controllers/todoController.js
+++ backend/controllers/todoController.js
@@ -31,8 +31,8 @@ exports.updateTodo = async (req, res) => {
 };
 
 // Delete a todo
-// exports.deleteTodo = async (req, res) => {
-//   const { id } = req.params;
-//   await Todo.findByIdAndDelete(id);
-//   res.json({ message: "Todo deleted" });
-// };
+exports.deleteTodo = async (req, res) => {
+  const { id } = req.params;
+  await Todo.findByIdAndDelete(id);
+  res.json({ message: "Todo deleted successfully" });
+};
