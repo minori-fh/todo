ef0832ca4929bc8979598906ba5f78659008f308
Minori Hashimoto <minori.fh@gmail.com>
2024-10-30 17:32:01 -0700

    remove demo contents

--- backend/controllers/todoController.js
+++ backend/controllers/todoController.js
@@ -29,10 +29,3 @@ exports.updateTodo = async (req, res) => {
   );
   res.json(updatedTodo);
 };
-
-// delete to do
-exports.deleteTodo = async (req, res) => {
-  const { id } = req.params;
-  await Todo.findByIdAndDelete(id);
-  res.json({ message: "Todo deleted successfully" });
-};
